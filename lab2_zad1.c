736b7dd37ed470faf5676da3f637d5f2b2c7cd9c
Kamil Bernacik <bernacikkamil@gmail.com>
2020-11-19 23:32:01 +0100

    Pierwsza zmiana

---
+++ lab2_zad1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int x;
+
+    scanf("%d", &x);
+    printf("%d\n", x);
+    printf("%d\n", ++x);
+    printf("%d\n", x);
+    printf("%d\n", x++);
+    printf("%d\n", x);
+    return 0;
+}
