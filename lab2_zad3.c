8a22c92fe3bc3624e4a8522e287546c3118d44cb
Kamil Bernacik <bernacikkamil@gmail.com>
2020-11-20 00:02:55 +0100

    Nowe zadanie i poprawki zadania trzeciego

--- lab2_zad3.c
+++ lab2_zad3.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
+int i=0;
+
 int main()
 {
-    //nic tu nie ma
+    printf("%d\n", i);
+
+    int i = 1;
+
+    printf("%d\n", i);
+    {
+        int i = 2;
+        printf ("%d\n",i);
+    }
+    printf("%d\n", i);
+    return 0;
 }
