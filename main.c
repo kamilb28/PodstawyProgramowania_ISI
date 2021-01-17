67570f55b51e2d32a4c5b65092c34159162aa5b8
Kamil Bernacik <bernacikkamil@gmail.com>
2021-01-17 21:43:41 +0100

    Zajęcia6

---
+++ main.c
@@ -0,0 +1,134 @@
+#include <stdio.h>
+
+void zadanie_1(){
+    //Funkcja, która kopiuje tablicę statyczną i zwraca
+    /*taką funkcją jest strcp :? */
+}
+void zadanie_2(){
+    //Zdefiniować funkcję push_back, która do istniejącej tablicy dynamicznej doda element na koniec i zwróci tablicę o jeden większą:
+    //    A: jak zrobić takie dodawanie elementów optymalniejszym?
+}
+void zadanie_3(){
+    //- Funkcja, która dla rozmiaru numeru zwróci wyzerowaną tablicę dynamiczną:
+    //    A: jak wyzerować tablicę statyczną bez pętli
+}
+void zadanie_4(){
+    //- Funkcja, która dla podanego numeru zwróci tablicę dynamiczną zawierającą kolejne liczby nieparzyste:
+    //    A: jak się robi kolejne liczby nieparzyste:
+}
+void zadanie_5(){
+    //- Funkcja, która dla podanego numeru zwróci tablicę dynamiczną zawierającą kolejne liczby z zakresu rosnącego:
+    //  -- Jeśli będzie również obsługa zakresu malejącego (od większego do mniejszego
+}
+void zadanie_6(){
+    //F. podwajająca tablicę i zwracająca podwojoną tablicę
+}
+void zadanie_7(){
+    //F. podwajająca samogłoski i zwracająca podwojoną tablicę
+}
+void zadanie_8(){
+    //!- Proszę o zdefiniowanie tablicy dynamicznej zbyt dużej i porównanie zachowania do tablic zmiennej długości: 2p i 1p
+    //! A: co się stanie jak nie uda się dynamiczna alokacja pamięci
+}
+void zadanie_9(){
+    //- Zdefiniować typ wyliczeniowy do dni tygodnia/miesięcy, oraz:
+    //    1-- Zrobić funkcję konwertującą wartość enum na tekst
+    //    A: jakie wartości mają poszczegulne wyliczenia w typie enum?
+}
+void zadanie_10(){
+    //2-- Zrobić funkcje konwertującą tekst na odpowiednią wartość enum
+    //    A: co się stanie gdy przypiszemy w C inną wartość, niż ma enum
+    //    A: jaki jest rozmiar typu enum?
+}
+void zadanie_11(){
+    // -- Zrobić funkcje, która wyświetli wszystkie wartości enum (możemy korzystać tylko ze standardowego C):
+    //    A: jak można sprawdzić ile jest takich wartości?
+}
+void zadanie_12(){
+    //--* Zrób program, który wczytuje dzień i aktywność (tekst),
+    // oraz potem można przeszukiwać swoje aktywności na dany dzień
+}
+void zadanie_13(){
+    //- F. wyświetlającą wiadomość poprzedzoną informacją jakiej
+    // rangi jest ta wiadomość ("[ERROR]", "[WARNING]", ...),
+    // typ ma być podawany jako argument funkcji w postaci typu enum
+}
+void zadanie_14(){
+    //2- Funkcja, która zmienia tekst na duże/małe litery
+    //- Funkcja, która zmienia małe na duże i duże na małe litery:
+}
+void zadanie_15(){
+    //2- F. zwracająca ilośc małych liter w tekście:
+}
+void zadanie_16(){
+    //2- F. przycinająca tekst do określonej długości (w razie gdy tekst jest za krótki nic nie robiąca):
+}
+void zadanie_17(){
+    //:D 1- Proszę o zaimplementowanie funkcji flegmatyk,
+    // która tekst zwróci ze zwielokrotnionymi samogłoskami: 2p i 1,5p i 1p (jeśli losową ilość zwielokratniamy to +1 punkty)
+}
+void zadanie_18(){
+    //3:D 1- Proszę o zaimplementowanie funkcji jakającej,
+    //która tekst zwróci ze zwielokrotnionymi sylabami: 3p i 2,5p i 2p
+    // (jeśli losową ilość zwielokratniamy to +1 punkty)
+}
+void zadanie_19(){
+    //Funkcja, która imię i nazwisko połączy w jeden tekst (wyświetlamy dopiero podłączone)
+}
+int main(void){
+    //---- Dynamiczna allokacja ----
+    //zadanie_1();
+    zadanie_2();
+    zadanie_3();
+    zadanie_4();
+    zadanie_5();
+    zadanie_6();
+    zadanie_7();
+    zadanie_8();
+    zadanie_9();
+    //---- Enum ----
+    zadanie_10();
+    zadanie_11();
+    zadanie_12();
+    zadanie_13();
+    zadanie_14();
+    //--- operacje tekstowe ---
+    zadanie_15();
+    zadanie_16();
+    zadanie_17();
+    zadanie_18();
+    zadanie_19();
+    return 0;
+}
+
+
+
+
+//3- F. łącząca imię i liczbę w jeden tekst (wyświetlamy dopiero podłączone)
+//2,3- Skopiowanie z tekstu zawierającego imię i nazwisko do dwóch oddzienych tablic (w jednej samo imię, w drugiej samo nazwisko)
+//------ suffixy:
+//A: co to za liczba: 10e6
+//A: co to za liczba 12L
+//A: co to za liczba 12LL
+//A: co to za liczba 12LL
+//! - proszę porównać: -1 < 0u i co wychodzi: printf("%d", -1 < 0u);
+//A: jak się mają te liczby: 0xf i 15 i 017
+//1,2 A: co to jest "martwa wołowina", ang. deadbeef
+//---- struktury:
+//1!- Zaimplementuj dwie strukruty  struct S1
+//    {
+//        bool b; //
+//        double d; //
+//        char c; //
+//        int i; //
+//    };
+//    struct S2
+//    {
+//        double d; //
+//        int i; //
+//        bool b; //
+//        char c; //
+//    };
+//    i porównaj rozmiary.
+//    A: jak zainicjalizować strukturę
+
